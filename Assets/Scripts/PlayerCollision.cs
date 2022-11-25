a041ae01883e413ce079658f2a4121df8fecb7b2
rushish <99073483+rushish@users.noreply.github.com>
2022-11-25 10:27:47 +0530

    First Commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    
+    void OnCollisionEnter (UnityEngine.Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag =="Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+   public Rigidbody rb;
+   public float forwardForce = 20000f;
+   public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0,forwardForce * Time.deltaTime);
+    }
+    void Update()
+    {
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+         if(Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+    }
+
+}
+
